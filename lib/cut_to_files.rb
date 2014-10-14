5c9152fdb2fac828823167a61e14376fbacee630
u89012 <u89012@users.noreply.github.com>
2014-10-14 13:35:29 -0400

    file modes added
    create new files using *
    append to existing files using +

--- lib/cut_to_files.rb
+++ lib/cut_to_files.rb
@@ -1,17 +1,34 @@
-require 'fileutils'
+require 'pathname'
 
 class String
 	def cut_to_files
-		f = $stdout
+		buf = Hash.new { |hash, key| hash[key] =  []}
+
+		cur = nil
 		self.lines.map{|l|
-			if l =~ /^--(.*)\/(.*)\s*$/
-				f.close if f != $stdout
-				FileUtils.mkdir_p $1
-				f = File.open($1 + '/' + $2, 'w')
+			l.strip!
+
+			if l =~ /^(\*|\+).+$/
+				cur = l
 				next
 			end
-			f.puts l
+
+			if cur
+				buf[cur] << l
+			end
 		}
-		f.close
+
+		# pp buf
+
+		buf.map{|k,v|
+			p = Pathname.new k[1..-1]
+			p.dirname.mkpath
+
+			if k[0] == "*"
+				open(p.to_s, "w"){|f| f << v.join($/)}
+			elsif k[0] == "+"
+				open(p.to_s, "a"){|f| f << v.join($/)}
+			end
+		}
 	end
 end
